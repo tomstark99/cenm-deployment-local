3fcf9861b2141e4414f761a237b429dff602f53c
tomstark99 <tom.stark@r3.com>
2023-03-02 15:51:43 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+#!/bin/bash -e
 git clone git@github.com:tomstark99/cenm-deployment-local.git
-(cd cenm-deployment-local && python3 setup_script.py)
-rm install.sh
+cat << EOL
+To continue with the setup, edit your env file in cenm-deployment-local/.env
+
+    $ mv .env.template .env
+    $ vim .env
+    
+    1. ARTIFACTORY_USERNAME=<some>.<one>@r3.com
+    2. ARTIFACTORY_API_KEY=<api_key>
+    3. AUTH_VERSION=<auth_version>
+    4. GATEWAY_VERSION=<gateway_version>
+    5. CENM_VERSION=<cenm_version>
+
+Once you are done with this, come back to this script and
+EOL
+read -p "press enter to continue"
+(cd cenm-deployment-local && python3 setup_script.py --setup-dir-structure --generate-certs)
+
+exec rm ./install.sh
