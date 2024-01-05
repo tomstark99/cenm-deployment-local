a84247380afe3d05b0567f182c56247212295dbd
tomstark99 <tom.stark@r3.com>
2024-01-05 13:27:17 +0000

    Added angel service

--- .src/managers/download_manager.py
+++ .src/managers/download_manager.py
@@ -35,16 +35,23 @@ class DownloadManager:
     def _get_md5sum(self, url: str) -> str:
         return self.sysi.run_get_stdout(f'curl -L --progress-bar -u {self.username}:{self.password} {url} | md5sum | cut -d " " -f 1')
 
-    def validate_download(self, url: str) -> bool:
+    def _validate_download(self, url: str) -> bool:
         """Check the md5sum of the downloaded file against the one that would be downloaded
 
-        TODO: currently this doesn't work due to item location not being in the root where the script is
-        there is a file not found error.
+        Args:
+            url:
+                The url to download the file from.
+
+        Returns:
+            True if the md5sums do not match, False otherwise.
         
         """
         artifact = url.split("/")[-1]
         print(f'Validating {artifact}')
-        return self.sysi.run_get_stdout(f'md5sum {artifact} | cut -d " " -f 1') != self._get_md5sum(url)
+        return not self.check_md5sum(artifact, url)
+
+    def check_md5sum(self, artifact_path: str, artifact_url: str) -> bool:
+        return self.sysi.run_get_stdout(f'md5sum {artifact_path} | cut -d " " -f 1') == self._get_md5sum(artifact_url)
 
     def download(self, url: str) -> bool:
         """Download a file from a given url using wget or curl.
@@ -68,3 +75,4 @@ class DownloadManager:
             if cmd != 0:
                 return True
         return False
+        # return not self._validate_download(url)
