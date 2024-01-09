f4f1f004ebd458b9f61e56a586976a4ba36c71e2
Tom Stark <47384103+tomstark99@users.noreply.github.com>
2024-01-09 11:03:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 printf "Cloning deployment repo...\n"
-git clone --branch=release/1.5 git@github.com:tomstark99/cenm-deployment-local.git --quiet
+git clone git@github.com:tomstark99/cenm-deployment-local.git --quiet
 (cd cenm-deployment-local && cp .env.template .env)
 
 cat << EOL
